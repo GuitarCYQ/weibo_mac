7c3fea69a067823d484ccbf948a92a2874d2ca28
GuitarCYQ <1159543426@qq.com>
2020-10-14 11:45:37 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('signup','UsersController@create')->name('signup');
 
+Route::resource('users','UsersController');
+
 
