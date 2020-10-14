ceced3931629f7e2a5dd181201ee0308e74a25b0
GuitarCYQ <1159543426@qq.com>
2020-10-14 12:40:23 +0000

    7.2创建会话

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,10 @@ Route::get('signup','UsersController@create')->name('signup');
 
 Route::resource('users','UsersController');
 
+//登录退出
+Route::get('login','SessionsController@create')->name('login');
+Route::post('login','SessionsController@store')->name('login');
+Route::delete('logout','SessionsController@destroy')->name('logout');
+
+
 
